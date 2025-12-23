e46b589256c2adbcdf1bbcdbc09305a4cf6723e5
Mukesh015 <rg37090@gmail.com>
2025-12-23 16:00:51 +0530

    done

--- db.js
+++ db.js
@@ -3,25 +3,24 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "root_password_here",
-  database: "DSP",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
 // export const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT || 3306),
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
+//   host: "127.0.0.1",
+//   port: 3306,
+//   user: "root",
+//   password: "root_password_here",
+//   database: "DSP",
 //   waitForConnections: true,
 //   connectionLimit: 10,
-//   queueLimit: 0
+//   queueLimit: 0,
 // });
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
